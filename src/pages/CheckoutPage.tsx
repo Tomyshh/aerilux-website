88830cfdd4989595e12a87edbb01bc5e14044e16
tomyshh <jami.ito@hotmail.com>
2026-02-01 16:46:52 +0200

    checkout: empêche le fond blanc de l'autofill navigateur

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -101,6 +101,18 @@ const Input = styled.input<{ $hasError?: boolean }>`
     color: rgba(255, 255, 255, 0.22);
     -webkit-text-fill-color: rgba(255, 255, 255, 0.22);
   }
+
+  /* Empêcher le fond blanc de l'autofill navigateur */
+  &:-webkit-autofill,
+  &:-webkit-autofill:hover,
+  &:-webkit-autofill:focus,
+  &:-webkit-autofill:active {
+    -webkit-box-shadow: 0 0 0 1000px rgba(30, 30, 30, 1) inset !important;
+    box-shadow: 0 0 0 1000px rgba(30, 30, 30, 1) inset !important;
+    -webkit-text-fill-color: #ffffff !important;
+    caret-color: #ffffff !important;
+    transition: background-color 5000s ease-in-out 0s;
+  }
 `;
 
 const ReadOnlyInput = styled(Input)`
